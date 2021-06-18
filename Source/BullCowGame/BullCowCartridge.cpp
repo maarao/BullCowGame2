3cca9b12496f7604d9d2a067a07ac2eb600811cc
zenM49 <58347499+zenM49@users.noreply.github.com>
2021-06-18 18:32:26 -0400

    Finished initial game loop

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,21 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Guess the 4-letter word.")); // TODO: Magic number remove!!!!!
+    PrintLine(TEXT("Press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
+    FString HiddenWord = TEXT("abduct"); // TODO: Move outside this function
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You win!"));
+    }
+    else
+    {
+        PrintLine(TEXT("You lost!"));
+    }
 }
