3a292e0d000f24df573f3859fca46f2e13cba9f9
zenM49 <58347499+zenM49@users.noreply.github.com>
2021-06-20 16:04:07 -0400

    Pseudo coded a bunch of the game loop

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,21 +4,31 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    HiddenWord = TEXT("abduct");
+    HiddenWord = TEXT("abduct"); // Sets HiddenWord
+    Lives = HiddenWord.Len(); // Sets lives
     PrintLine(TEXT("Welcome to Bull Cows!"));
-    PrintLine(TEXT("Guess the 4-letter word.")); // TODO: Magic number remove!!!!!
+    PrintLine(TEXT("Guess the 6-letter word.")); // TODO: Magic number remove!!!!!
     PrintLine(TEXT("Press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
+
+    // Checks guess
     if (Input == HiddenWord)
     {
         PrintLine(TEXT("You win!"));
     }
     else
     {
-        PrintLine(TEXT("You lost!"));
+        PrintLine(TEXT("You lost!"));   
     }
+    // Check if isogram
+    // Check if right number of characters
+    // Remove Life
+
+    // Check if lives > 0
+    // Yes -> guess again
+    // No -> you lose; Hit enter to play again
 }
